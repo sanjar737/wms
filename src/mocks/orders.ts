a29d122c3b85ed52d9473bb2f4d274e552c04d35
sanjar737 <ylent2014@gmail.com>
2021-05-19 19:27:32 +0500

    add mocks

---
+++ src/mocks/orders.ts
@@ -0,0 +1,86 @@
+export default [
+  {
+    orderId: 1,
+    deliveryInfo: {
+      deliveryPoint: {
+        id: 1,
+        shortName: "НЧЛ-ВХТ",
+      },
+    },
+    customer: {
+      customerId: 1,
+      firstName: "Иван",
+      lastName: "Иванов",
+    },
+  },
+  {
+    orderId: 2,
+    deliveryInfo: {
+      deliveryPoint: {
+        id: 1,
+        shortName: "НЧЛ-ВХТ",
+      },
+    },
+    customer: {
+      customerId: 1,
+      firstName: "Иван",
+      lastName: "Иванов",
+    },
+  },
+  {
+    orderId: 3,
+    deliveryInfo: {
+      deliveryPoint: {
+        id: 1,
+        shortName: "НЧЛ-ВХТ",
+      },
+    },
+    customer: {
+      customerId: 1,
+      firstName: "Иван",
+      lastName: "Иванов",
+    },
+  },
+  {
+    orderId: 4,
+    deliveryInfo: {
+      deliveryPoint: {
+        id: 1,
+        shortName: "НЧЛ-ВХТ",
+      },
+    },
+    customer: {
+      customerId: 1,
+      firstName: "Иван",
+      lastName: "Иванов",
+    },
+  },
+  {
+    orderId: 5,
+    deliveryInfo: {
+      deliveryPoint: {
+        id: 2,
+        shortName: "ЕКБ-1",
+      },
+    },
+    customer: {
+      customerId: 2,
+      firstName: "Петр",
+      lastName: "Петров",
+    },
+  },
+  {
+    orderId: 6,
+    deliveryInfo: {
+      deliveryPoint: {
+        id: 1,
+        shortName: "НЧЛ-ВХТ",
+      },
+    },
+    customer: {
+      customerId: 1,
+      firstName: "Иван",
+      lastName: "Иванов",
+    },
+  },
+];
